c22bf977f511a93a287344c18e1ab0fdd1d262c5
Helene Rigner <hvs@qlik.com>
2018-08-31 07:56:06 +0200

    updating test script to match the dir

--- test/test.sh
+++ test/test.sh
@@ -3,11 +3,11 @@ DIR=`dirname $0`
 
 go build -o $DIR/qlix $DIR/../main.go
 
-./qlix --config $DIR/project1/qli.yml reload > $DIR/output/project1-reload.txt
-./qlix --config $DIR/project1/qli.yml fields > $DIR/output/project1-fields.txt
-./qlix --config $DIR/project1/qli.yml field numbers > $DIR/output/project1-field-numbers.txt
-./qlix --config $DIR/project2/qli.yml reload > $DIR/output/project2-reload.txt
-./qlix --config $DIR/project2/qli.yml fields > $DIR/output/project2-fields.txt
+$DIR/qlix --config $DIR/project1/qli.yml reload > $DIR/output/project1-reload.txt
+$DIR/qlix --config $DIR/project1/qli.yml fields > $DIR/output/project1-fields.txt
+$DIR/qlix --config $DIR/project1/qli.yml field numbers > $DIR/output/project1-field-numbers.txt
+$DIR/qlix --config $DIR/project2/qli.yml reload > $DIR/output/project2-reload.txt
+$DIR/qlix --config $DIR/project2/qli.yml fields > $DIR/output/project2-fields.txt
 
 git --no-pager diff --ignore-all-space --word-diff=porcelain -- $DIR/output
 
