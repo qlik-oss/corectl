9db9f727fa76b6b676ee52829ed92b04a1ca4562
Gabriel Weitoft (gwe) <gabriel.weitoft@qlik.com>
2018-09-19 13:36:20 +0200

    Fixed tests

--- test/corectl_integration_test.go
+++ test/corectl_integration_test.go
@@ -21,7 +21,15 @@ var update = flag.Bool("update", false, "update golden files")
 
 var engineIP = flag.String("engineIP", "localhost:9076", "dir of package containing embedded files")
 
-const binaryName = "corectl"
+func getBinaryName() string {
+	if runtime.GOOS == "windows" {
+		return "corectl.exe"
+	} else {
+		return "corectl"
+	}
+}
+
+var binaryName = getBinaryName()
 
 var binaryPath string
 
@@ -78,6 +86,7 @@ func TestCorectl(t *testing.T) {
 		{"reload project1", []string{"--config=test/project1/qli.yml", connectToEngine, "reload"}, "project1-reload.golden"},
 		{"fields project 1", []string{"--config=test/project1/qli.yml ", connectToEngine, "fields"}, "project1-fields.golden"},
 		{"field numbers project 1", []string{"--config=test/project1/qli.yml ", connectToEngine, "field", "numbers"}, "project1-field-numbers.golden"},
+		{"eval project 1", []string{"--config=test/project2/qli.yml ", connectToEngine, "eval", "count(numbers)", "by", "xyz"}, "project1-eval-1.golden"},
 		{"reload project 2", []string{"--config=test/project2/qli.yml ", connectToEngine, "reload"}, "project2-reload.golden"},
 		{"fields project 2", []string{"--config=test/project2/qli.yml ", connectToEngine, "fields"}, "project2-fields.golden"},
 	}
@@ -120,7 +129,7 @@ func TestMain(m *testing.M) {
 
 	binaryPath = abs
 
-	if err := exec.Command("make").Run(); err != nil {
+	if err := exec.Command("go", "build", "-o", "corectl.exe", "-v").Run(); err != nil {
 		fmt.Printf("could not make binary for %s: %v", binaryName, err)
 		os.Exit(1)
 	}
