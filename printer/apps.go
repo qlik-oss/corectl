65bfebbcec605ee37f3963219e3d2de386862706
Henrik Carlioth <carlioth@users.noreply.github.com>
2018-10-12 15:06:54 +0200

    Print id when listing apps (#69)
    * adding id for print of apps

--- printer/apps.go
+++ printer/apps.go
@@ -11,9 +11,9 @@ import (
 func PrintApps(docList []*enigma.DocListEntry) {
 
 	docTable := tm.NewTable(0, 10, 3, ' ', 0)
-	fmt.Fprintf(docTable, "Name\tLast-Reloaded\tReadOnly\tTitle\n")
+	fmt.Fprintf(docTable, "Id\tName\tLast-Reloaded\tReadOnly\tTitle\n")
 	for _, doc := range docList {
-		fmt.Fprintf(docTable, "%s\t%s\t%t\t%s\n", doc.DocName, doc.LastReloadTime, doc.ReadOnly, doc.Title)
+		fmt.Fprintf(docTable, "%s\t%s\t%s\t%t\t%s\n", doc.DocId, doc.DocName, doc.LastReloadTime, doc.ReadOnly, doc.Title)
 	}
 	fmt.Print(docTable)
 }
