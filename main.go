e9f11238985a6e852356f7f2b04acdd02e6ca14a
Håkan Axelsson <han@qlik.com>
2019-03-13 10:28:54 +0100

    Fix for setting version while building (#150)

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/qlik-oss/corectl/cmd"
 )
 
+// version will be set with: go build -ldflags "-X main.version=X.Y.Z"
+var version = "development build"
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(version)
 }
