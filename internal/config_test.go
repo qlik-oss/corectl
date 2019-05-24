c8daaf530fa421fd29ea7bc74de13507a7ce762a
Marcus de Lacerda <dxa@qlik.com>
2019-05-24 13:17:43 +0200

    Added unit test for env sub

---
+++ internal/config_test.go
@@ -0,0 +1,46 @@
+package internal
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+)
+var source = []byte(`test: ${_TEST1_}
+nested:
+  nest2:
+    name: foo
+    name2: foo
+    test2: ${_TEST2_}
+  test3: ${_TEST3_}
+list:
+  - ${_TEST4_}
+`)
+
+func TestEnvVarSubstitution(t *testing.T) {
+	config := readConfig(source)
+	fmt.Println(config)
+	err := subEnvVars(config)
+	assert.Error(t, err)
+	os.Setenv("_TEST1_", "TEST1")
+	err = subEnvVars(readConfig(source))
+	assert.Error(t, err)
+	os.Setenv("_TEST2_", "TEST2")
+	err = subEnvVars(readConfig(source))
+	assert.Error(t, err)
+	os.Setenv("_TEST3_", "TEST3")
+	err = subEnvVars(readConfig(source))
+	// We don't substitute env-variables in lists
+	assert.Nil(t, err)
+}
+
+func readConfig(source []byte) (config *map[interface{}]interface{}) {
+	config = &(map[interface{}]interface{}{})
+	if err := yaml.Unmarshal(source, config); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return
+}
