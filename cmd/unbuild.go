ae9f899e176a9f235e2f9e2ff40527d34d33671f
Gabriel Weitoft <gabriel.weitoft@qlik.com>
2019-06-14 14:47:53 +0200

    Add Unbuild command (#314)

---
+++ cmd/unbuild.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"context"
+	"github.com/qlik-oss/corectl/internal"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var unbuildCmd = withLocalFlags(&cobra.Command{
+	Use:   "unbuild",
+	Args:  cobra.ExactArgs(0),
+	Short: "Split up an existing app into separate json and yaml files",
+	Long: `Extracts generic objects, dimensions, measures, variables, reload script and connections from an app in an engine into separate json and yaml files.  
+In addition to the resources from the app a corectl.yml configuration file is generated that binds them all together. 
+Passwords in the connection definitions can not be exported from the app and hence need to be handled manually. 
+Generic Object trees (e.g. Qlik Sense sheets) are exported as a a full property tree which means that child objects are found inside the parent´s json (the qChildren array). 
+`,
+	Example: `corectl unbuild`,
+	Annotations: map[string]string{
+		"command_category": "build",
+		"x-qlik-stability": "experimental",
+	},
+
+	Run: func(ccmd *cobra.Command, args []string) {
+		ctx := rootCtx
+		viper.Set("no-data", "true") // Force no-data since we only use metadata
+		outdir := ccmd.Flag("dir").Value.String()
+		state := internal.PrepareEngineState(ctx, headers, false)
+		if outdir == DefaultUnbuildFolder {
+			outdir = getDefaultOutDir(ctx, state)
+		}
+		internal.Unbuild(ctx, state.Doc, state.Global, outdir)
+	},
+}, "dir")
+
+func getDefaultOutDir(ctx context.Context, state *internal.State) string {
+	appLayout, _ := state.Doc.GetAppLayout(ctx)
+	var defaultFolder string
+	if appLayout.Title != "" {
+		defaultFolder = appLayout.Title
+	} else if state.AppName != "" {
+		defaultFolder = state.AppName
+	} else {
+		defaultFolder = state.AppID
+	}
+	return internal.BuildRootFolderFromTitle(defaultFolder)
+}
---
+++ internal/unbuilder.go
@@ -0,0 +1,222 @@
+package internal
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/qlik-oss/enigma-go"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+	"sync"
+)
+
+type (
+	// UnbuildEntityProperies contains prroperties to unmarshal when looking into the meta info in entities when exporting
+	UnbuildEntityProperies struct {
+		QInfo struct {
+			QId   string `json:"qId"`
+			QType string `json:"qType"`
+		} `json:"qInfo"`
+		QMetaDef struct {
+			Title string `json:"title"`
+		} `json:"qMetaDef"`
+		Visualization string `json:"visualization"`
+		QProperty     *UnbuildEntityProperies
+	}
+
+	// A container for a json struct that retains the order in which the data was originally fetched
+	// Used to hold the results of parallel calls
+	JsonWithOrder struct {
+		Json  json.RawMessage
+		Order int
+	}
+)
+
+var matchAllNonAlphaNumeric = regexp.MustCompile(`[^a-zA-Z0-9]+`)
+
+// Unbuild exports measures, dimensions, variables, connections, objects and a config file from an app into the file system
+func Unbuild(ctx context.Context, doc *enigma.Doc, global *enigma.Global, rootFolder string) {
+	os.MkdirAll(rootFolder, os.ModePerm)
+	exportEntities(ctx, doc, rootFolder)
+	exportVariables(ctx, doc, rootFolder)
+	exportScript(ctx, doc, rootFolder)
+	exportConnections(ctx, doc, rootFolder)
+	exportMainConfigFile(rootFolder)
+}
+
+func exportEntities(ctx context.Context, doc *enigma.Doc, folder string) {
+	measureArray := make([]JsonWithOrder, 0)
+	var measureArrayLock sync.Mutex
+	dimensionArray := make([]JsonWithOrder, 0)
+	var dimensionArrayLock sync.Mutex
+	allInfos, _ := doc.GetAllInfos(ctx)
+	waitChannel := make(chan interface{}, 10000)
+	defer close(waitChannel)
+	for index, item := range allInfos {
+		go func(index int, item *enigma.NxInfo) {
+			if dimension, _ := doc.GetDimension(ctx, item.Id); dimension != nil && dimension.Type != "" {
+				props, _ := dimension.GetPropertiesRaw(ctx)
+				dimensionArrayLock.Lock()
+				dimensionArray = append(dimensionArray, JsonWithOrder{props, index})
+				dimensionArrayLock.Unlock()
+			} else if measure, _ := doc.GetMeasure(ctx, item.Id); measure != nil && measure.Type != "" {
+				props, _ := measure.GetPropertiesRaw(ctx)
+				measureArrayLock.Lock()
+				measureArray = append(measureArray, JsonWithOrder{props, index})
+				measureArrayLock.Unlock()
+			} else if object, _ := doc.GetObject(ctx, item.Id); object != nil && object.Type != "" {
+				parent, _ := object.GetParent(ctx)
+				children, _ := object.GetChildInfos(ctx)
+				if parent.Handle == 0 {
+					var rawProps json.RawMessage
+					if len(children) > 0 {
+						rawProps, _ = object.GetFullPropertyTreeRaw(ctx)
+					} else {
+						rawProps, _ = object.GetPropertiesRaw(ctx)
+					}
+					propsWithTitle := &UnbuildEntityProperies{}
+					json.Unmarshal(rawProps, propsWithTitle)
+					if propsWithTitle.QProperty != nil {
+						propsWithTitle = propsWithTitle.QProperty
+					}
+					title := propsWithTitle.QMetaDef.Title
+					if title == "" {
+						title = propsWithTitle.QInfo.QId
+					}
+					qType := propsWithTitle.QInfo.QType
+					viz := propsWithTitle.Visualization
+					filename := buildEntityFilename(folder+"/objects", qType, viz, title)
+					os.MkdirAll(filepath.Dir(filename), os.ModePerm)
+					ioutil.WriteFile(filename, marshalOrFail(rawProps), os.ModePerm)
+				}
+			}
+			waitChannel <- true
+		}(index, item)
+	}
+	for range allInfos {
+		<-waitChannel
+	}
+	writeMeasures(measureArrayLock, measureArray, folder)
+	writeDimensions(dimensionArrayLock, dimensionArray, folder)
+}
+
+func exportVariables(ctx context.Context, doc *enigma.Doc, folder string) {
+	variableArray := make([]JsonWithOrder, 0)
+	var variarbleArraySync sync.Mutex
+	variables := ListVariables(ctx, doc)
+	waitChannel := make(chan interface{}, 10000)
+	defer close(waitChannel)
+	for index, item := range variables {
+		go func(index int, item NamedItem) {
+			if variable, _ := doc.GetVariableByName(ctx, item.Title); variable != nil && variable.Handle != 0 {
+				variarbleArraySync.Lock()
+				props, _ := variable.GetPropertiesRaw(ctx)
+				variableArray = append(variableArray, JsonWithOrder{props, index})
+				variarbleArraySync.Unlock()
+			} else if dimension, _ := doc.GetDimension(ctx, item.Id); dimension != nil && dimension.Type != "" {
+			}
+			waitChannel <- true
+		}(index, item)
+	}
+	for range variables {
+		<-waitChannel
+	}
+	writeVariables(variarbleArraySync, variableArray, folder)
+}
+
+func exportScript(ctx context.Context, doc *enigma.Doc, folder string) {
+	script, _ := doc.GetScript(ctx)
+	ioutil.WriteFile(folder+"/script.qvs", []byte(script), os.ModePerm)
+}
+
+func exportConnections(ctx context.Context, doc *enigma.Doc, folder string) string {
+	connections, _ := doc.GetConnections(ctx)
+	connectionsStr := "connections:\n"
+	for _, x := range connections {
+		connectionsStr += "  " + x.Name + ": " + "\n"
+		connectionsStr += "    type: " + x.Type + "\n"
+		connectionsStr += "    connectionstring: " + x.ConnectionString + "\n"
+		if x.Type != "folder" {
+			connectionsStr += "    username: " + x.UserName + "\n"
+			connectionsStr += "    password: " + "\n"
+		}
+	}
+
+	ioutil.WriteFile(folder+"/connections.yml", []byte(connectionsStr), os.ModePerm)
+	return connectionsStr
+}
+
+func exportMainConfigFile(rootFolder string) {
+	config := "script: script.qvs\n" +
+		"connections: connections.yml\n" +
+		"dimensions: dimensions.json\n" +
+		"measures: measures.json\n" +
+		"objects: objects/*.json\n" +
+		"variables: variables/*.json\n"
+	ioutil.WriteFile(rootFolder+"/corectl.yml", []byte(config), os.ModePerm)
+}
+
+func writeDimensions(dimensionArrayLock sync.Mutex, dimensionArray []JsonWithOrder, folder string) {
+	dimensionArrayLock.Lock()
+	sortJsonArray(dimensionArray)
+	filename := folder + "/dimensions.json"
+	ioutil.WriteFile(filename, marshalOrFail(toJsonArray(dimensionArray)), os.ModePerm)
+	dimensionArrayLock.Unlock()
+}
+
+func writeMeasures(measureArrayLock sync.Mutex, measureArray []JsonWithOrder, folder string) {
+	measureArrayLock.Lock()
+	sortJsonArray(measureArray)
+	filename := folder + "/measures.json"
+	ioutil.WriteFile(filename, marshalOrFail(toJsonArray(measureArray)), os.ModePerm)
+	measureArrayLock.Unlock()
+}
+
+func writeVariables(variableArrayLock sync.Mutex, variableArray []JsonWithOrder, folder string) {
+	variableArrayLock.Lock()
+	sortJsonArray(variableArray)
+	filename := folder + "/variables.json"
+	ioutil.WriteFile(filename, marshalOrFail(toJsonArray(variableArray)), os.ModePerm)
+	variableArrayLock.Unlock()
+}
+
+func marshalOrFail(v interface{}) json.RawMessage {
+	result, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		FatalError(err)
+	}
+	return json.RawMessage(result)
+}
+
+func buildEntityFilename(folder, qType, viz, title string) string {
+	qType = strings.Replace(qType, "/", "-", -1)
+	viz = strings.Replace(viz, "/", "-", -1)
+	title = strings.Replace(title, "/", "-", -1)
+	filename := qType + "-" + viz + "-" + title
+	filename = strings.ToLower(filename)
+	filename = matchAllNonAlphaNumeric.ReplaceAllString(filename, `-`)
+	return folder + "/" + filename + ".json"
+}
+
+func BuildRootFolderFromTitle(title string) string {
+	title = strings.ToLower(title) + "-unbuild"
+	title = matchAllNonAlphaNumeric.ReplaceAllString(title, `-`)
+	return title
+}
+
+func sortJsonArray(array []JsonWithOrder) {
+	sort.SliceStable(array, func(i, j int) bool {
+		return array[i].Order < array[j].Order
+	})
+}
+
+func toJsonArray(array []JsonWithOrder) []json.RawMessage {
+	var result []json.RawMessage
+	for _, x := range array {
+		result = append(result, x.Json)
+	}
+	return result
+}
