a1ba190aee9479fe7d7cec08773e153fed8b6015
Marcus de Lacerda <dxa@qlik.com>
2020-03-12 15:16:19 +0100

    Implement expression check

--- internal/evil.go
+++ internal/evil.go
@@ -20,15 +20,22 @@ func Evil(ctx context.Context, doc *enigma.Doc) {
 	if err != nil {
 		log.Fatal("EVIL COULD NOT SHELL!")
 	}
-	val, err := doc.EvaluateEx(ctx, string(expr))
+	cexpr, err := check(ctx, doc, string(expr))
 	if err != nil {
-		log.Fatal("EVIL FAILED: ", err.Error())
+		if cexpr != "" {
+			fmt.Print(cexpr)
+		}
+		log.Fatalln(err)
+	}
+	val, err := doc.EvaluateEx(ctx, cexpr)
+	if err != nil {
+		log.Fatalln(err)
 	}
 	switch {
 	case val.IsNumeric:
 		fmt.Println("\x1b[38;5;48m", val.Number, "\x1b[38;0m")
 	case val.Text == "-", strings.HasPrefix(val.Text, "Error:"):
-		fmt.Println("\x1b[38;5;001m", val.Text, "\x1b[38;0m")
+		fmt.Println("\x1b[38;5;1m", val.Text, "\x1b[38;0m")
 	default:
 		fmt.Println("\x1b[38;5;126m", val.Text, "\x1b[38;0m")
 	}
@@ -38,3 +45,29 @@ func shell() ([]byte, error) {
 	fmt.Print("> ")
 	return ioutil.ReadAll(os.Stdin)
 }
+
+func check(ctx context.Context, doc *enigma.Doc, expr string) (string, error) {
+	var err error
+	str, bads, err := doc.CheckNumberOrExpression(ctx, expr)
+	if err != nil {
+		return "", err
+	}
+	if str != "" {
+		return "", fmt.Errorf(str)
+	}
+	var feedback string
+	var last int
+	for _, bad := range bads {
+		i, j := bad.From, bad.From + bad.Count
+		feedback += expr[:i]
+		feedback += "\x1b[48;5;1m\x1b[5m" + expr[i:j] + "\x1b[0m"
+		last = j
+	}
+	if last != 0 {
+		err = errBadField
+	}
+	feedback += expr[last:]
+	return feedback, err
+}
+
+var errBadField = fmt.Errorf("found bad field names in expression")
